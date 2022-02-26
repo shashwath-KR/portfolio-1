d6b3f439e53d28eea5ef18dbe19379a5e9fc23fb
shashwath-KR <shashwathgowda93@gmail.com>
2022-02-26 14:22:39 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import {Link} from 'react-scroll';
 const App = () => {
   
   return (
-     <section className= 'container'>
+     <section id= 'app' className= 'container'>
      <div className= 'btn'>
-        <button id= 'app'>Home</button>
+        <button >Home</button>
         <Link to= 'about' smooth= {true} duration= {1501} ><button>About</button></Link>
         <Link to= 'skills' smooth= {true} duration= {1500} ><button>Skills</button></Link>
         <Link to= 'achieve' smooth= {true} duration= {1500} ><button>Certificates</button></Link>
         <Link to= 'contact' smooth= {true} duration= {1500} ><button>Contact</button></Link>
         </div>
-        <div>
+        <div >
         <img src= {frontPage} alt= 'Lap' width= '100%' height=  '100%' />
         </div>
         <About />
